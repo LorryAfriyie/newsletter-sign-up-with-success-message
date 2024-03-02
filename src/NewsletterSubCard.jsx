0d2d65ae43d21f26787f4fa590a9b6d834416568
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-03-02 04:39:29 +0200

    Removing default boilerplate
    Removing vite + react boilerplate code

---
+++ src/NewsletterSubCard.jsx
@@ -0,0 +1,10 @@
+export const NewsletterSubCard = () => {
+  return (
+    <div className="newsletter-card">
+      <div className="card-row">
+        <div className="card-col">Hello</div>
+        <div className="card-col">World</div>
+      </div>
+    </div>
+  );
+};
