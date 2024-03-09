2459aa539843a275ecaf17a5847f21e37ad479f3
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-03-09 05:19:29 +0200

    List item array
    Creating a list item array to iterate through to display card messages.

--- src/NewsletterSubCard.jsx
+++ src/NewsletterSubCard.jsx
@@ -1,15 +1,26 @@
 export const NewsletterSubCard = () => {
+  const listItems = [
+    { id: 1, message: "Product discovery and building what matters" },
+    { id: 2, message: "Measuring to ensure updates are a success" },
+    { id: 3, message: "And much more!" },
+  ];
+
   return (
     <div className="newsletter-card">
       <div className="card">
         <div className="col-info">
           <h1>Stay Updated!</h1>
-          <p>Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul>
-            <li>Hello</li>
-            <li>World</li>
-          </ul>
+
+          <div className="sub-text">
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+              {listItems.map((x) => {
+                return <li key={x.id}>{x.message}</li>;
+              })}
+            </ul>
+          </div>
         </div>
+
         <div className="col-img"></div>
       </div>
     </div>
