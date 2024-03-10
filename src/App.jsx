c48fcde65925233c7dd886f5ffbecf4c85520759
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-03-10 03:18:22 +0200

    Code refactor
    Changing internal card layout from display flex into display grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ function App() {
   return (
     <>
       <NewsletterSubCard />
+      <div className="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Lawrence Afriyie</a>.
+      </div>
     </>
   );
 }
--- src/NewsletterSubCard.jsx
+++ src/NewsletterSubCard.jsx
@@ -7,21 +7,35 @@ export const NewsletterSubCard = () => {
 
   return (
     <div className="newsletter-card">
-      <div className="card">
-        <div className="col-info">
-          <h1>Stay Updated!</h1>
+      <div className="container">
+        <form className="subscribe-form">
+          <h1>Stay updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul>
+            {listItems.map((x) => {
+              return <li key={x.id}>{x.message}</li>;
+            })}
+          </ul>
 
-          <div className="sub-text">
-            <p>Join 60,000+ product managers receiving monthly updates on:</p>
-            <ul>
-              {listItems.map((x) => {
-                return <li key={x.id}>{x.message}</li>;
-              })}
-            </ul>
+          <div className="form-control">
+            <label htmlFor="email">
+              <small>Email address</small>
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email@company.com"
+            />
           </div>
-        </div>
 
-        <div className="col-img"></div>
+          <button type="submit">Subscribe to monthly newsletter</button>
+        </form>
+
+        <img
+          src="/images/illustration-sign-up-desktop.svg"
+          alt="illustration-sign-up-desktop.svg"
+        />
       </div>
     </div>
   );
